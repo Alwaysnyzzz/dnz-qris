c8f1624f03dac3075d061e5244d05609200acb43
Alwaysnyzzz <masdenys34@gmail.com>
2026-03-02 15:16:33 +0700

    Fix: Beneran iki qris-generate su

--- api/create.js
+++ api/create.js
@@ -1,22 +1,37 @@
 import { createClient } from '@supabase/supabase-js';
-import qris from 'qris-payload';
+import qris from 'qris-generate';
 
 const supabase = createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+  
   try {
     const { nominal } = req.body;
+    // Tambah kode unik 1-5 perak
     const unik = Math.floor(Math.random() * 5) + 1; 
     const total = parseInt(nominal) + unik;
+
+    // Simpan ke database
     const { data, error } = await supabase
       .from('transaksi')
       .insert([{ nominal: total, status: 'PENDING' }])
       .select();
+
     if (error) throw error;
+
+    // Kode QRIS Statis Denz_Store
     const qrisStatis = "00020101021126570011ID.DANA.WWW011893600915397838615002099783861500303UMI51440014ID.CO.QRIS.WWW0215ID10254266189280303UMI5204594553033605802ID5910Denz_Store600412926105613166304E74A";
+
+    // GENERATE QRIS OTOMATIS
     const qrisDinamis = qris(qrisStatis, { amount: total });
-    res.status(200).json({ orderId: data[0].id, total: total, qrString: qrisDinamis });
+
+    res.status(200).json({ 
+      orderId: data[0].id, 
+      total: total, 
+      qrString: qrisDinamis 
+    });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
