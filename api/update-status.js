f2a3777f9067ea13844d7c605f8ddd3c5e57a8f1
Alwaysnyzzz <masdenys34@gmail.com>
2026-03-02 23:42:46 +0700

    Fix: Security update, use secrets su

--- api/update-status.js
+++ api/update-status.js
@@ -1,21 +1,25 @@
 import { createClient } from '@supabase/supabase-js';
-
 const supabase = createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
   const { nominal, secret_key } = req.body;
-  if (secret_key !== "denz_rahasia_123") return res.status(403).json({ error: 'Akses ditolak' });
+
+  // Cek secret key teko Env Vercel
+  if (secret_key !== process.env.MY_SECRET_KEY) {
+    return res.status(403).json({ error: 'Akses ditolak' });
+  }
 
   try {
     const { data, error } = await supabase
-        .from('transaksi')
-        .update({ status: 'SUCCESS' })
-        .eq('nominal', parseInt(nominal))
-        .eq('status', 'PENDING');
+      .from('transaksi')
+      .update({ status: 'SUCCESS' })
+      .eq('nominal', parseInt(nominal))
+      .eq('status', 'PENDING')
+      .select();
 
     if (error) throw error;
-    res.status(200).json({ message: 'Database Diperbarui!' });
+    res.status(200).json({ message: "Update Sukses su!" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
